a09472c406e6aea02671bface568936c5189baf4
zwg3 <115691096+zwg3@users.noreply.github.com>
2024-01-17 00:29:12 +0300

    Added Lab2

---
+++ Lab2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath =
+                new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddPolygon(new Point[] { new Point(0, 0),
+                new Point(0, this.Height),
+                new Point(this.Width, 0) });
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
